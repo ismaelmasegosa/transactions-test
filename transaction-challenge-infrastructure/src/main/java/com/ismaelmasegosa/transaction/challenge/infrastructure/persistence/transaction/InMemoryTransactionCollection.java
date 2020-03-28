9112313b270d48013dfd8433f25281c11e5fbc85
Ismael Masegosa Fernández-Bermejo <ismaelmfe@ext.zara.com>
2020-03-28 20:39:13 +0100

    + Refactored InMemoryTransactionCollection

--- transaction-challenge-infrastructure/src/main/java/com/ismaelmasegosa/transaction/challenge/infrastructure/persistence/transaction/InMemoryTransactionCollection.java
+++ transaction-challenge-infrastructure/src/main/java/com/ismaelmasegosa/transaction/challenge/infrastructure/persistence/transaction/InMemoryTransactionCollection.java
@@ -3,6 +3,7 @@ package com.ismaelmasegosa.transaction.challenge.infrastructure.persistence.tran
 import com.ismaelmasegosa.transaction.challenge.domain.transaction.Transaction;
 import com.ismaelmasegosa.transaction.challenge.domain.transaction.TransactionCollection;
 import com.ismaelmasegosa.transaction.challenge.infrastructure.persistence.transaction.entities.TransactionEntity;
+import java.util.function.Function;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,16 +11,28 @@ public class InMemoryTransactionCollection implements TransactionCollection {
 
   private final TransactionRepository transactionRepository;
 
-  public InMemoryTransactionCollection(TransactionRepository transactionRepository) {
+  private final Function<TransactionEntity, Transaction> entityToDomain;
+
+  private final Function<Transaction, TransactionEntity> domainToEntity;
+
+  public InMemoryTransactionCollection(TransactionRepository transactionRepository, Function<TransactionEntity, Transaction> entityToDomain,
+      Function<Transaction, TransactionEntity> domainToEntity) {
     this.transactionRepository = transactionRepository;
+    this.entityToDomain = entityToDomain;
+    this.domainToEntity = domainToEntity;
   }
 
   @Override
   public Transaction addTransaction(Transaction transaction) {
-    TransactionEntity transactionEntity =
-        new TransactionEntity(transaction.getReference(), transaction.getAccountIban(), transaction.getDate(), transaction.getAmount(),
-            transaction.getFee(), transaction.getDescription());
-    transactionRepository.save(transactionEntity);
-    return transaction;
+    TransactionEntity savedTransaction = transactionRepository.save(mapToEntity(transaction));
+    return mapToDomain(savedTransaction);
+  }
+
+  private Transaction mapToDomain(TransactionEntity transactionEntity) {
+    return entityToDomain.apply(transactionEntity);
+  }
+
+  private TransactionEntity mapToEntity(Transaction transaction) {
+    return domainToEntity.apply(transaction);
   }
 }
