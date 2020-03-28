aff6c203f3176c824332aaf5a73be82723a5fe4e
Ismael Masegosa Fernández-Bermejo <ismaelmfe@ext.zara.com>
2020-03-28 19:42:58 +0100

    + Implemented create transaction usecase

--- transaction-challenge-usecase/src/main/java/com/ismaelmasegosa/transaction/challenge/usecases/CreateTransaction.java
+++ transaction-challenge-usecase/src/main/java/com/ismaelmasegosa/transaction/challenge/usecases/CreateTransaction.java
@@ -1,15 +1,28 @@
 package com.ismaelmasegosa.transaction.challenge.usecases;
 
+import static com.ismaelmasegosa.transaction.challenge.domain.core.Either.right;
+
 import com.ismaelmasegosa.transaction.challenge.domain.core.Either;
 import com.ismaelmasegosa.transaction.challenge.domain.core.Error;
 import com.ismaelmasegosa.transaction.challenge.domain.transaction.Transaction;
+import com.ismaelmasegosa.transaction.challenge.domain.transaction.TransactionCollection;
 import com.ismaelmasegosa.transaction.challenge.usecases.core.UseCase;
 import com.ismaelmasegosa.transaction.challenge.usecases.params.CreateTransactionParams;
 
 public class CreateTransaction implements UseCase<CreateTransactionParams, Either<Error, Transaction>> {
 
+  private final TransactionCollection accountCollection;
+
+  public CreateTransaction(TransactionCollection accountCollection) {
+    this.accountCollection = accountCollection;
+  }
+
   @Override
   public Either<Error, Transaction> execute(CreateTransactionParams params) {
-    throw new UnsupportedOperationException("Not implemented");
+    Transaction transaction =
+        new Transaction(params.getReference(), params.getAccountIban(), params.getDate(), params.getAmount(), params.getFee(),
+            params.getDescription());
+    Transaction transactionSaved = accountCollection.addTransaction(transaction);
+    return right(transactionSaved);
   }
 }
