9a1648331cf80b5c1c01ed899b78ccf5edf25d1f
Ismael Masegosa Fernández-Bermejo <ismaelmfe@ext.zara.com>
2020-03-29 23:29:32 +0200

    + Implemented search transaction controller

--- transaction-challenge-infrastructure/src/main/java/com/ismaelmasegosa/transaction/challenge/infrastructure/rest/mapper/TransactionDtoToCreateTransactionParams.java
+++ transaction-challenge-infrastructure/src/main/java/com/ismaelmasegosa/transaction/challenge/infrastructure/rest/mapper/TransactionDtoToCreateTransactionParams.java
@@ -2,6 +2,8 @@ package com.ismaelmasegosa.transaction.challenge.infrastructure.rest.mapper;
 
 import com.ismaelmasegosa.transaction.challenge.infrastructure.rest.dto.TransactionDto;
 import com.ismaelmasegosa.transaction.challenge.usecases.params.CreateTransactionParams;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.ZoneOffset;
 import java.util.function.Function;
 import org.springframework.stereotype.Component;
@@ -12,6 +14,11 @@ public class TransactionDtoToCreateTransactionParams implements Function<Transac
   @Override
   public CreateTransactionParams apply(TransactionDto dto) {
     long date = dto.getDate() != null ? dto.getDate().toInstant(ZoneOffset.UTC).toEpochMilli() : System.currentTimeMillis();
-    return new CreateTransactionParams(dto.getReference(), dto.getAccountIban(), date, dto.getAmount(), dto.getFee(), dto.getDescription());
+    return new CreateTransactionParams(dto.getReference(), dto.getAccountIban(), date, roundToTwodDecimals(dto.getAmount()),
+        roundToTwodDecimals(dto.getFee()), dto.getDescription());
+  }
+
+  public double roundToTwodDecimals(double amount) {
+    return new BigDecimal(amount).setScale(2, RoundingMode.HALF_EVEN).doubleValue();
   }
 }
