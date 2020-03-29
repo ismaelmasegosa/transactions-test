5982bb227011fd285cbd0ac19b857b58874b121e
Ismael Masegosa Fernández-Bermejo <ismaelmfe@ext.zara.com>
2020-03-29 22:48:59 +0200

    + Implemented search transactions use case

--- transaction-challenge-usecase/src/test/java/com/ismaelmasegosa/transaction/challenge/usecases/SearchTransactionsTest.java
+++ transaction-challenge-usecase/src/test/java/com/ismaelmasegosa/transaction/challenge/usecases/SearchTransactionsTest.java
@@ -7,18 +7,20 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 import com.ismaelmasegosa.transaction.challenge.domain.core.Either;
-import com.ismaelmasegosa.transaction.challenge.domain.core.Validation;
+import com.ismaelmasegosa.transaction.challenge.domain.core.Error;
 import com.ismaelmasegosa.transaction.challenge.domain.transaction.Transaction;
 import com.ismaelmasegosa.transaction.challenge.domain.transaction.TransactionCollection;
 import com.ismaelmasegosa.transaction.challenge.usecases.params.SearchTransactionsParams;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.Test;
 
 public class SearchTransactionsTest {
 
   TransactionCollection transactionCollection = mock(TransactionCollection.class);
 
-  SearchTransactions searchTransactions = new SearchTransactions();
+  SearchTransactions searchTransactions = new SearchTransactions(transactionCollection);
 
   @Test
   public void given_A_Account_IBAN_And_Ascending_Sort_When_The_Search_Transactions_Use_Case_Is_Executed_Then_The_Transactions_Should_Be_Returned() {
@@ -27,18 +29,20 @@ public class SearchTransactionsTest {
     int sort = 1;
     SearchTransactionsParams params = new SearchTransactionsParams(accountIban, sort);
     List<Transaction> transactions = createdTransationsList();
-    given(transactionCollection.findByAccountIbanOrderByAmount(accountIban, sort)).willReturn(transactions);
+    List<Transaction> ascendingList =
+        transactions.stream().sorted(Comparator.comparingDouble(Transaction::getAmount)).collect(Collectors.toList());
+    given(transactionCollection.findByAccountIbanOrderByAmount(accountIban, sort)).willReturn(ascendingList);
 
     // when
-    Either<Validation, List<Transaction>> response = searchTransactions.execute(params);
+    Either<Error, List<Transaction>> response = searchTransactions.execute(params);
 
     // then
     assertTrue(response.isRight());
     List<Transaction> transactionsResponse = response.get();
     assertEquals(4, transactionsResponse.size());
-    assertEquals(transactionsResponse.get(0).getReference(), "33333A");
-    assertEquals(transactionsResponse.get(1).getReference(), "44444A");
-    assertEquals(transactionsResponse.get(2).getReference(), "11111A");
-    assertEquals(transactionsResponse.get(3).getReference(), "22222A");
+    assertEquals(transactionsResponse.get(0).getReference(), "22222A");
+    assertEquals(transactionsResponse.get(1).getReference(), "11111A");
+    assertEquals(transactionsResponse.get(2).getReference(), "44444A");
+    assertEquals(transactionsResponse.get(3).getReference(), "33333A");
   }
 }
