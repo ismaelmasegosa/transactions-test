4d986f6c96b5f03ee55c382529097cc49a5bdf78
Ismael Masegosa Fernández-Bermejo <ismaelmfe@ext.zara.com>
2020-03-29 23:40:54 +0200

    + Implemented use case test for the operation search transactions sorted descending by quantity

--- transaction-challenge-usecase/src/test/java/com/ismaelmasegosa/transaction/challenge/usecases/SearchTransactionsTest.java
+++ transaction-challenge-usecase/src/test/java/com/ismaelmasegosa/transaction/challenge/usecases/SearchTransactionsTest.java
@@ -45,4 +45,28 @@ public class SearchTransactionsTest {
     assertEquals(transactionsResponse.get(2).getReference(), "44444A");
     assertEquals(transactionsResponse.get(3).getReference(), "33333A");
   }
+
+  @Test
+  public void given_A_Account_IBAN_And_Descending_Sort_When_The_Search_Transactions_Use_Case_Is_Executed_Then_The_Transactions_Should_Be_Returned() {
+    // given
+    String accountIban = "ES9820385778983000760236";
+    int sort = -1;
+    SearchTransactionsParams params = new SearchTransactionsParams(accountIban, sort);
+    List<Transaction> transactions = createdTransationsList();
+    List<Transaction> ascendingList =
+        transactions.stream().sorted(Comparator.comparingDouble(Transaction::getAmount).reversed()).collect(Collectors.toList());
+    given(transactionCollection.findByAccountIbanOrderByAmount(accountIban, sort)).willReturn(ascendingList);
+
+    // when
+    Either<Error, List<Transaction>> response = searchTransactions.execute(params);
+
+    // then
+    assertTrue(response.isRight());
+    List<Transaction> transactionsResponse = response.get();
+    assertEquals(4, transactionsResponse.size());
+    assertEquals(transactionsResponse.get(0).getReference(), "33333A");
+    assertEquals(transactionsResponse.get(1).getReference(), "44444A");
+    assertEquals(transactionsResponse.get(2).getReference(), "11111A");
+    assertEquals(transactionsResponse.get(3).getReference(), "22222A");
+  }
 }
