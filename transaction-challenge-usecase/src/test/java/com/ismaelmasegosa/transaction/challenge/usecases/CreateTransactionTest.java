93fde483be78be89a969fbb2d1b6087d8650c8aa
Ismael Masegosa Fernández-Bermejo <ismaelmfe@ext.zara.com>
2020-03-29 19:12:45 +0200

    + Implemented failed test to verify the reference generation

--- transaction-challenge-usecase/src/test/java/com/ismaelmasegosa/transaction/challenge/usecases/CreateTransactionTest.java
+++ transaction-challenge-usecase/src/test/java/com/ismaelmasegosa/transaction/challenge/usecases/CreateTransactionTest.java
@@ -3,6 +3,7 @@ package com.ismaelmasegosa.transaction.challenge.usecases;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -10,6 +11,7 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.springframework.util.StringUtils.isEmpty;
 
 import com.ismaelmasegosa.transaction.challenge.domain.account.AccountBalanceProvider;
 import com.ismaelmasegosa.transaction.challenge.domain.core.Either;
@@ -39,7 +41,7 @@ public class CreateTransactionTest {
   }
 
   @Test
-  public void given_A_Transaction_When_The_Create_Transaction_Use_Case_Is_Executed_Then_The_Created_Transacton_Should_Be_Returned() {
+  public void given_A_Transaction_When_The_Create_Transaction_Use_Case_Is_Executed_Then_The_Created_Transaction_Should_Be_Returned() {
     // given
     long date = System.currentTimeMillis();
     String reference = "12345A";
@@ -112,5 +114,28 @@ public class CreateTransactionTest {
     Error errorValidation = response.getLeft();
     assertEquals(400, errorValidation.getStatusCode());
     assertEquals(errors, errorValidation.getErrors());
+  }
+
+  @Test
+  public void given_A_Transaction_Without_Reference_When_The_Create_Transaction_Use_Case_Is_Executed_Then_The_Created_Transaction_With_Reference_Should_Be_Returned() {
+    // given
+    List<String> errors = singletonList("Transaction not created, the total account balance can not be bellow zero");
+    long date = System.currentTimeMillis();
+    String reference = null;
+    String accountIban = "ES9820385778983000760236";
+    final double amount = 1678.87;
+    final double fee = 3.18;
+    final String description = "Restaurant payment";
+    CreateTransactionParams params = new CreateTransactionParams(reference, accountIban, date, amount, fee, description);
+    Transaction transaction = new Transaction(reference, accountIban, date, amount, fee, description);
+    given(transactionCollection.addTransaction(any(Transaction.class))).willReturn(transaction);
+
+    // when
+    Either<Error, Transaction> response = createTransaction.execute(params);
+
+    // then
+    assertTrue(response.isRight());
+    Transaction transactionResponse = response.get();
+    assertFalse(isEmpty(transactionResponse.getReference()));
   }
 }
