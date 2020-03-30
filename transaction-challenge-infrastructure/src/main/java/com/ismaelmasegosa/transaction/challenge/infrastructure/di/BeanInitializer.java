bca7272ac88db8e291490f207a1ee5ca0405d54b
Ismael Masegosa Fernández-Bermejo <ismaelmfe@ext.zara.com>
2020-03-30 09:55:13 +0200

    + Implemented search transactions controller

--- transaction-challenge-infrastructure/src/main/java/com/ismaelmasegosa/transaction/challenge/infrastructure/di/BeanInitializer.java
+++ transaction-challenge-infrastructure/src/main/java/com/ismaelmasegosa/transaction/challenge/infrastructure/di/BeanInitializer.java
@@ -7,8 +7,11 @@ import com.ismaelmasegosa.transaction.challenge.domain.events.DomainEventPublish
 import com.ismaelmasegosa.transaction.challenge.domain.transaction.Transaction;
 import com.ismaelmasegosa.transaction.challenge.domain.transaction.TransactionCollection;
 import com.ismaelmasegosa.transaction.challenge.usecases.CreateTransaction;
+import com.ismaelmasegosa.transaction.challenge.usecases.SearchTransactions;
 import com.ismaelmasegosa.transaction.challenge.usecases.core.UseCase;
 import com.ismaelmasegosa.transaction.challenge.usecases.params.CreateTransactionParams;
+import com.ismaelmasegosa.transaction.challenge.usecases.params.SearchTransactionsParams;
+import java.util.List;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,5 +24,12 @@ public class BeanInitializer {
   public UseCase<CreateTransactionParams, Either<Error, Transaction>> createTransaction(DomainEventPublisher eventPublisher,
       AccountBalanceProvider accountBalanceProvider, TransactionCollection transactionCollection) {
     return new CreateTransaction(eventPublisher, accountBalanceProvider, transactionCollection);
+  }
+
+  @Bean
+  @ConditionalOnMissingBean(name = "searchTransactions")
+  public UseCase<SearchTransactionsParams, Either<Error, List<Transaction>>> searchTransactions(
+      TransactionCollection transactionCollection) {
+    return new SearchTransactions(transactionCollection);
   }
 }
--- transaction-challenge-infrastructure/src/main/java/com/ismaelmasegosa/transaction/challenge/infrastructure/rest/mapper/TransactionDtoToCreateTransactionParams.java
+++ transaction-challenge-infrastructure/src/main/java/com/ismaelmasegosa/transaction/challenge/infrastructure/rest/mapper/TransactionDtoToCreateTransactionParams.java
@@ -1,7 +1,10 @@
 package com.ismaelmasegosa.transaction.challenge.infrastructure.rest.mapper;
 
+import static java.math.RoundingMode.HALF_EVEN;
+
 import com.ismaelmasegosa.transaction.challenge.infrastructure.rest.dto.TransactionDto;
 import com.ismaelmasegosa.transaction.challenge.usecases.params.CreateTransactionParams;
+import java.math.BigDecimal;
 import java.time.ZoneOffset;
 import java.util.function.Function;
 import org.springframework.stereotype.Component;
@@ -12,6 +15,11 @@ public class TransactionDtoToCreateTransactionParams implements Function<Transac
   @Override
   public CreateTransactionParams apply(TransactionDto dto) {
     long date = dto.getDate() != null ? dto.getDate().toInstant(ZoneOffset.UTC).toEpochMilli() : System.currentTimeMillis();
-    return new CreateTransactionParams(dto.getReference(), dto.getAccountIban(), date, dto.getAmount(), dto.getFee(), dto.getDescription());
+    return new CreateTransactionParams(dto.getReference(), dto.getAccountIban(), date, roundToTwodDecimals(dto.getAmount()), dto.getFee(),
+        dto.getDescription());
+  }
+
+  public double roundToTwodDecimals(double amount) {
+    return new BigDecimal(amount).setScale(2, HALF_EVEN).doubleValue();
   }
 }
