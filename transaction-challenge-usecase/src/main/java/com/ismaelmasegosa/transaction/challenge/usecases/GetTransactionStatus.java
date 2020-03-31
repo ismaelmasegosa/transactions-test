022c9d1306297e9c50d8c2a6f89e1b3cd3c6f7b5
Ismael Masegosa Fernández-Bermejo <ismaelmfe@ext.zara.com>
2020-03-31 02:44:25 +0200

    * Refactored use case to validate params

--- transaction-challenge-usecase/src/main/java/com/ismaelmasegosa/transaction/challenge/usecases/GetTransactionStatus.java
+++ transaction-challenge-usecase/src/main/java/com/ismaelmasegosa/transaction/challenge/usecases/GetTransactionStatus.java
@@ -5,6 +5,8 @@ import static com.ismaelmasegosa.transaction.challenge.domain.core.Either.right;
 
 import com.ismaelmasegosa.transaction.challenge.domain.core.Either;
 import com.ismaelmasegosa.transaction.challenge.domain.core.Error;
+import com.ismaelmasegosa.transaction.challenge.domain.core.Validation;
+import com.ismaelmasegosa.transaction.challenge.domain.core.error.BadRequestError;
 import com.ismaelmasegosa.transaction.challenge.domain.core.error.TransactionNotFoundError;
 import com.ismaelmasegosa.transaction.challenge.domain.transaction.Transaction;
 import com.ismaelmasegosa.transaction.challenge.domain.transaction.TransactionCollection;
@@ -12,6 +14,7 @@ import com.ismaelmasegosa.transaction.challenge.domain.transaction.status.Transa
 import com.ismaelmasegosa.transaction.challenge.domain.transaction.status.TransactionsStatusProvider;
 import com.ismaelmasegosa.transaction.challenge.usecases.core.UseCase;
 import com.ismaelmasegosa.transaction.challenge.usecases.params.GetTransactionStatusParams;
+import java.util.List;
 import java.util.Optional;
 
 public class GetTransactionStatus implements UseCase<GetTransactionStatusParams, Either<Error, TransactionStatus>> {
@@ -27,12 +30,22 @@ public class GetTransactionStatus implements UseCase<GetTransactionStatusParams,
 
   @Override
   public Either<Error, TransactionStatus> execute(GetTransactionStatusParams params) {
-    Optional<Transaction> optionalTransaction = transactionCollection.findByReference(params.getReference());
-    if (optionalTransaction.isPresent()) {
-      Transaction transaction = optionalTransaction.get();
-      return right(transactionsStatusProvider.getTransactionStatus(transaction, params.getChannel()));
+    final Validation validationError = params.validate();
+    if (validationError.hasErrors()) {
+      return left(createBadRequestResponse(validationError.getErrors()));
     } else {
-      return left(new TransactionNotFoundError(params.getReference()));
+      Optional<Transaction> optionalTransaction = transactionCollection.findByReference(params.getReference());
+      if (optionalTransaction.isPresent()) {
+        Transaction transaction = optionalTransaction.get();
+        return right(transactionsStatusProvider.getTransactionStatus(transaction, params.getChannel()));
+      } else {
+        return left(new TransactionNotFoundError(params.getReference()));
+      }
     }
   }
+
+  private BadRequestError createBadRequestResponse(List<String> errors) {
+    return new BadRequestError(errors);
+  }
+
 }
